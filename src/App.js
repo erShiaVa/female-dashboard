510744fed0544b6943a99057dd2d1c3441f86751
erShiaVa <vakili.ershia@gmail.com>
2024-01-23 15:44:58 +0100

    feature_pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CoacheeHome from "./pages/CoacheeHome";
+import LogIn from "./pages/LogIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import CoacheeProfile from "./pages/CoacheeProfile";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CoacheeHome/>} />
+        <Route path="/log-in" element={<LogIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/coachee-profile" element={<CoacheeProfile/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/CoacheeProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoacheeProfile() {
+  return (
+    <div>CoacheeProfile</div>
+  )
+}
+
+export default CoacheeProfile
